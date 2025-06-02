d012b22a3d66691bc892f90e1c319fb8fd496d58
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-02 19:22:25 +0700

    menambahkan fungsi main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -18,3 +18,19 @@ public:
      }
 };
 
+int main()
+{
+    remotelampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "lampu Dapur");
+
+    cout <<  lampuRumah.getSaklarNo(0) << endl;
+    cout <<  lampuRumah.getSaklarNo(1) << endl;
+    cout <<  lampuRumah.getSaklarNo(2) << endl;
+    cout <<  lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
